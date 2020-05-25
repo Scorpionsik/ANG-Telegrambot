825c302b5f6aea70277c644e7c2f7664cb4bac10
Alex Coreman <scorpionsik@gmail.com>
2020-05-25 16:33:42 +0300

    Make more complex root_dir

--- html/telegram/bot.php
+++ html/telegram/bot.php
@@ -1,6 +1,5 @@
 <?php
-$root_dir = explode('html',__DIR__)[0];
-$root_dir = "${root_dir}html";
+$root_dir = explode('html',__DIR__)[0] . html;
 
 include "givemyprecious.php";
 require_once "${root_dir}/vendor/autoload.php";
