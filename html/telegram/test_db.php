78198f3abfaf50137d0671ee25b0d35fec19ed76
Alex Coreman <scorpionsik@gmail.com>
2020-05-26 14:01:32 +0300

    Add db test

---
+++ html/telegram/test_db.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'givemyprecious.php'; // подключаем скрипт
+ 
+// подключаемся к серверу
+$link = mysqli_connect($host, $dblogin, $dbpassw, $database) 
+    or die("Ошибка " . mysqli_error($link));
+ 
+if ($link->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $link->connect_errno . ") " . $link->connect_error;
+}
+ 
+// закрываем подключение
+mysqli_close($link);
+?>
