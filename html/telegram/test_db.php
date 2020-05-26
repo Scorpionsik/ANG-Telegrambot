ee408f1dbcdc21a65f3cdb425325d728fc607551
Alex Coreman <scorpionsik@gmail.com>
2020-05-26 14:13:29 +0300

    test db arain

--- html/telegram/test_db.php
+++ html/telegram/test_db.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'givemyprecious.php'; // подключаем скрипт
+require_once "givemyprecious.php";
  
 // подключаемся к серверу
 $link = mysqli_connect($host, $dblogin, $dbpassw, $database) 
