0e4ff8579cc775a890be47f032f19e1fdb3f3d81
Alex Coreman <scorpionsik@gmail.com>
2020-05-26 15:38:23 +0300

    Finish mysql example

--- html/telegram/test_db.php
+++ html/telegram/test_db.php
@@ -1,14 +1,25 @@
 <?php
 require_once "givemyprecious.php";
- 
+
 // подключаемся к серверу
-$link = mysqli_connect($host, $dblogin, $dbpassw, $database) 
-    or die("Ошибка " . mysqli_error($link));
- 
+
+$link = new mysqli($host, $dblogin, $dbpassw, $database); 
+
 if ($link->connect_errno) {
     echo "Не удалось подключиться к MySQL: (" . $link->connect_errno . ") " . $link->connect_error;
 }
- 
+else echo "Подключились!<br/><br/>";
+/*
+$query = "INSERT INTO test_user (Iduser,Status) VALUES (1,1);";
+
+$result = mysqli_query($link,$query) or die("Error: " . mysqli_error($link));
+if($result)
+{
+    echo "Done!";
+}
+*/
+
 // закрываем подключение
 mysqli_close($link);
+echo "<br/><br/>Подключение закрыто!";
 ?>
