82d8332491d654a09829e997f570977cfa209078
Alex Coreman <scorpionsik@gmail.com>
2020-05-27 14:37:38 +0300

    Test delete user message

--- html/telegram/bot.php
+++ html/telegram/bot.php
@@ -10,7 +10,7 @@ $bot = new \TelegramBot\Api\Client(${token});
 $bot->command('start', function ($message) use ($bot) {
 	include "connection.php";
 	$dblink = new mysqli($host, $dblogin, $dbpassw, $database); 
-	$id = $message->getChat()->getId();
+	$id_user = $message->getChat()->getId();
 	$query = "SELECT * FROM test_user where Iduser=${id};";
 	
 	$result = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
@@ -20,13 +20,13 @@ $bot->command('start', function ($message) use ($bot) {
 		
 		if($row)
 		{
-			$bot->sendMessage($id, 'С возвращением, ' . $row[3] . "!");
+			$bot->sendMessage($id_user, 'С возвращением, ' . $row[3] . "!");
 		}
 		else
 		{
-			mysqli_query($dblink,"INSERT INTO test_user (Iduser,Status) VALUES ($id,0);") or die("Ошибка: " . mysqli_error($dblink));
-			$bot->sendMessage($id, 'Добро пожаловать!');	
-			$bot->sendMessage($id, 'Пожалуйста, напиши своё имя:');
+			mysqli_query($dblink,"INSERT INTO test_user (Iduser,Status) VALUES ($id_user,0);") or die("Ошибка: " . mysqli_error($dblink));
+			$bot->sendMessage($id_user, 'Добро пожаловать!');	
+			$bot->sendMessage($id_user, 'Пожалуйста, напиши своё имя:');
 		}
 	}
 	mysqli_free_result($result);
@@ -40,8 +40,8 @@ $bot->command('help', function ($message) use ($bot) {
 /*
 //команда Id
 $bot->command('id', function ($message) use ($bot) {
-    $id = $message->getChat()->getId();
-    $bot->sendMessage($id,$id);
+    $id_user = $message->getChat()->getId();
+    $bot->sendMessage($id_user,$id_user);
 });*/
 
 //Обработка введенного текста
@@ -51,7 +51,13 @@ $bot->on(function ($Update) use ($bot) {
 	include "connection.php";
 	$dblink = new mysqli($host, $dblogin, $dbpassw, $database); 
 	$msg_text = htmlentities(mysqli_real_escape_string($dblink,$message->getText()));
-	$id = $message->getChat()->getId();
+	$chat = $message->getChat();
+	$id_user = $chat->getId();
+	
+	$id_user_message = $message->getMessageId();
+	$id_user_chat = $chat->getChatId();
+	$bot->deleteMessage($id_user_chat, $id_user_message);
+	
 	$query = "SELECT * FROM test_user where Iduser=${id};";
 	$result = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
 
@@ -64,21 +70,21 @@ $bot->on(function ($Update) use ($bot) {
 			if($row[2] == 0)
 			{
 				mysqli_query($dblink,"UPDATE test_user SET Status=1, Username='${msg_text}' WHERE Id=" . $row[0] . ";") or die("Ошибка: " . mysqli_error($dblink));
-				$bot->sendMessage($id, "Приятно познакомится, ${msg_text}!");
-				$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[['text'=>'test']]]);
-                $bot->sendMessage($id, "hello", null, false, null, $keyboard);
+				$bot->sendMessage($id_user, "Приятно познакомится, ${msg_text}!");
+				$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[['text'=>'➡️ test']]]);
+                $bot->sendMessage($id_user, "hello", null, false, null, $keyboard);
 			}
 			//Логика по умолчанию
 			else
 			{
-				$bot->sendMessage($id, "Ты написал: ${msg_text}");
+				$bot->sendMessage($id_user, "Ты написал: ${msg_text}");
 			}
 		}
 	}
 
 	mysqli_free_result($result);
 	mysqli_close($dblink);
-    //$bot->sendMessage($id, "Ты написал: " . $msg_text);
+    //$bot->sendMessage($id_user, "Ты написал: " . $msg_text);
 }, function () { return true; });
 
 $bot->run();
