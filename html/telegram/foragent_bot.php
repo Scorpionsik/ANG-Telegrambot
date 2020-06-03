5fc81d419bdef4f1dcb212e01f77aa3b903f8844
Alex Coreman <scorpionsik@gmail.com>
2020-06-03 16:16:22 +0300

    fix

--- html/telegram/foragent_bot.php
+++ html/telegram/foragent_bot.php
@@ -23,6 +23,7 @@ $bot->on(function ($Update) use ($bot) {
 	$msg_text = htmlentities(mysqli_real_escape_string($dblink,$message->getText()));
 	$id_user = $message->getChat()->getId();
 	$lock = true;
+	$result = null;
 	if($msg_text == "/start")
 	{
 		$query = "INSERT INTO telegram_users (Id_telegram_user) values (${id_user});";
