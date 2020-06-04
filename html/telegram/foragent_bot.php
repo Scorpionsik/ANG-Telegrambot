30f6618640aa9fc572933bfe48002cd64bc01320
Alex Coreman <scorpionsik@gmail.com>
2020-06-04 13:49:47 +0300

    fix

--- html/telegram/foragent_bot.php
+++ html/telegram/foragent_bot.php
@@ -18,7 +18,7 @@ $bot->on(function ($Update) use ($bot) {
 	
 	if($msg_text == "/help")
 	{
-		$bot->sendMessage($id_user, 'Если у вас возникли вопросы или ошибки при работе с ботом, напишите мне и подробно изложите суть вопроса или проблемы.\n\nХорошего дня и отличного настроения, будьте здоровы!');
+		$bot->sendMessage($id_user, 'Если у вас возникли вопросы или ошибки при работе с ботом, напишите мне и подробно изложите суть вопроса или проблемы.');
 		$bot->sendMessage($id_user, 'Хорошего дня и отличного настроения, будьте здоровы!');
 		$bot->sendContact($id_user,'+380951473711','Саша');
 	}
@@ -46,7 +46,11 @@ $bot->on(function ($Update) use ($bot) {
 					if(preg_match("/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/i",$msg_text))
 					{
 						//код проверки по белому листу
-						$bot->sendMessage($id_user, "Корректный номер!");
+						
+						$clear_phone = preg_replace("/\D/i","",$msg_text);
+						$bot->sendMessage($id_user, $clear_phone);
+						$clear_phone = preg_replace("/^[380]{0,3}/i",""$clear_phone);
+						$bot->sendMessage($id_user, $clear_phone);
 					}
 					else
 					{
@@ -57,13 +61,15 @@ $bot->on(function ($Update) use ($bot) {
 							$bot->sendMessage($id_user, $msg_text);
 						}
 					}
+					/*
 					$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(
 					[
 						[
 							['text'=>'Отправить номер с телеграма','request_contact'=>true]
 						]
-					]);
-					$bot->sendMessage($id_user, "Для подтверждения входа, введите свой рабочий номер телефона, пожалуйста!", null, false, null, $keyboard);
+					]);*/
+					//$bot->sendMessage($id_user, "Для подтверждения входа, введите свой рабочий номер телефона, пожалуйста!", null, false, null, $keyboard);
+					$bot->sendMessage($id_user, "Для подтверждения входа, введите свой рабочий номер телефона, пожалуйста!");
 				}
 				else
 				{
