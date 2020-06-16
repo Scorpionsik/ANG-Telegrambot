b2dc640a6597bedf60337b73a8e0a238c98754f1
Alex Coreman <scorpionsik@gmail.com>
2020-06-16 16:13:14 +0300

    edit get info, part 2

--- html/telegram/bot.php
+++ html/telegram/bot.php
@@ -9,7 +9,7 @@ $dblink = new mysqli($host, $dblogin, $dbpassw, $database);
 $bot = new \TelegramBot\Api\Client(${token});
 //$bot->sendMessage(425486413, 'Test');
 
-$query = "select telegram_users.Id_telegram_user as 'Telegram' from telegram_users join white_list using (Id_whitelist_user) WHERE telegram_users.Id_whitelist_user != 11";
+$query = "select telegram_users.Id_whitelist_user as 'Id', telegram_users.Id_telegram_user as 'Telegram' from telegram_users join white_list using (Id_whitelist_user) WHERE telegram_users.Id_whitelist_user != 11";
 $result = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
 if($result)
 {
@@ -19,7 +19,50 @@ if($result)
 		$row = mysqli_fetch_row($result);
 		if($row)
 		{
-			//$bot->sendMessage($row[0], 'Добрый день! Прошу вас проверить, приходит ли информация по вашему району из бота. Если нет, сообщите в Вайбер по номеру 095 147 37 11. Заранее вам спасибо!');
+			//show results code
+										$query = "select offers.Internal_id, types.Type_name, flat_types.Typename, offers.Locality, districts.District_name, offers.Address, offers.Description, offers.Room_counts, offers.Floor, offers.Floors_total, offers.Area, offers.Lot_area, offers.Living_space, offers.Kitchen_space, offers.Price, offers.Image_url from offers inner join bind_whitelist_distr_flats on offers.Id_type=bind_whitelist_distr_flats.Id_type and offers.Id_flat_type=bind_whitelist_distr_flats.Id_flat_type and offers.Id_district=bind_whitelist_distr_flats.Id_district and offers.Room_counts=bind_whitelist_distr_flats.Room_counts inner join types on offers.Id_type=types.Id_type inner join flat_types on offers.Id_flat_type=flat_types.Id_flat_type inner join districts on offers.Id_district=districts.Id_district where bind_whitelist_distr_flats.Id_whitelist_user=" . $row[0] . ";";
+										$result_bind = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
+										if($result_bind)
+										{
+											//--get info code--//
+											$row_bind_count = mysqli_num_rows($result_bind);
+											if($row_bind_count > 0)
+											{
+												for($i = 0; $i < $row_bind_count; $i++)
+												{
+													$row_bind = mysqli_fetch_row($result_bind);
+													
+													$keyboard_inline = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+														[
+															[
+																['text' => 'Ссылка на сайт', 'url' => 'http://an-gorod.com.ua/real/flat/sale?q=' . $row_bind[0]]
+															]
+														]
+													);
+													
+													$offer_message = $row_bind[0];
+													$offer_message = $offer_message . "\r\n" . $row_bind[2] . " " . $row_bind[7] . "-комнатная, " . $row_bind[1] . " \r\n" . $row_bind[3] . ", " . $row_bind[4];
+													if($row_bind[5] != null)
+													{
+														$offer_message = $offer_message . ", " . $row_bind[5];
+													}
+													$offer_message = $offer_message . " \r\n" . $row_bind[8] . "/" . $row_bind[9] . " \n" . $row_bind[10] . "/" . $row_bind[12] . "/" . $row_bind[13] . " \r\n \nЦена: " . $row_bind[14] . "\n\n" . $row_bind[6];
+													$bot->sendMessage($id_user, $offer_message, null, false, null, $keyboard_inline);
+													
+													
+												}
+												$bot->sendMessage($id_user, "Всего ${row_bind_count} объектов за последние 3 дня.");
+											}
+											else $bot->sendMessage($id_user, "Информации по вашему району на данный момент нет, попробуйте позже!", null, false, null, $keyboard);
+											//--end get info code--//
+										}
+										else
+										{
+											$bot->sendMessage($id_user, "Информации по вашему району на данный момент нет, попробуйте позже!", null, false, null, $keyboard);
+										}	
+										mysqli_free_result($result_bind);
+			
+			//$bot->sendMessage($row[1], 'Добрый день! Прошу вас проверить, приходит ли информация по вашему району из бота. Если нет, сообщите в Вайбер по номеру 095 147 37 11. Заранее вам спасибо!');
 		}
 	}
 	mysqli_free_result($result);
