000ba5a8c6bde8c3f530a168960c5b7981de6916
Alex Coreman <scorpionsik@gmail.com>
2020-06-16 15:33:25 +0300

    edit get info, part 2

--- html/telegram/foragent_bot.php
+++ html/telegram/foragent_bot.php
@@ -173,13 +173,10 @@ $bot->on(function ($Update) use ($bot) {
 									if($row_from_whitelist[3] == false)
 									{
 										//show results code
-										$bot->sendMessage($id_user, "test");
 										$query = "select offers.Internal_id, types.Type_name, flat_types.Typename, offers.Locality, districts.District_name, offers.Address, offers.Description, offers.Room_counts, offers.Floor, offers.Floors_total, offers.Area, offers.Lot_area, offers.Living_space, offers.Kitchen_space, offers.Price, offers.Image_url from offers inner join bind_whitelist_distr_flats on offers.Id_type=bind_whitelist_distr_flats.Id_type and offers.Id_flat_type=bind_whitelist_distr_flats.Id_flat_type and offers.Id_district=bind_whitelist_distr_flats.Id_district and offers.Room_counts=bind_whitelist_distr_flats.Room_counts inner join types on offers.Id_type=types.Id_type inner join flat_types on offers.Id_flat_type=flat_types.Id_flat_type inner join districts on offers.Id_district=districts.Id_district where bind_whitelist_distr_flats.Id_whitelist_user=" . $row_from_whitelist[0] . ";";
 										$result_bind = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
-										$bot->sendMessage($id_user, "get");
 										if($result_bind)
 										{
-											$bot->sendMessage($id_user, "start");
 											//--get info code--//
 											$row_bind_count = mysqli_num_rows($result_bind);
 											if($row_bind_count > 0)
@@ -187,7 +184,6 @@ $bot->on(function ($Update) use ($bot) {
 												for($i = 0; $i < $row_bind_count; $i++)
 												{
 													$row_bind = mysqli_fetch_row($result_bind);
-													$bot->sendMessage($id_user, $i);
 													$offer_message = $row_bind[0] . "\n" . $row_bind[2] . " " . $row_bind[7] . "-комнатная, " . $row_bind[1] . "\n" . $row_bind[3] + ", " . $row_bind[4];
 													
 													if($row_bind[5] != null)
