b6ce7a7d9f672876971e6e9d49a30f261289793c
Alex Coreman <scorpionsik@gmail.com>
2020-07-14 12:22:55 +0300

    start add new security rules

--- html/telegram/foragent_send_notifications.php
+++ html/telegram/foragent_send_notifications.php
@@ -38,17 +38,17 @@ if($result)
 										$query = "select offers.Internal_id, types.Type_name, flat_types.Typename, localities.Locality_name, districts.District_name, offers.Address, offers.Description, offers.Room_counts, offers.Floor, offers.Floors_total, offers.Area, offers.Lot_area, offers.Living_space, offers.Kitchen_space, offers.Price, offers.Image_url, offers.IsNew, offers.IsEdit, offers.Orient, offers.Entity_id from offers inner join bind_whitelist_distr_flats on offers.Id_type=bind_whitelist_distr_flats.Id_type AND offers.Id_locality=bind_whitelist_distr_flats.Id_locality AND (offers.Id_flat_type=bind_whitelist_distr_flats.Id_flat_type OR bind_whitelist_distr_flats.Id_flat_type=1) AND (offers.Id_district=bind_whitelist_distr_flats.Id_district OR bind_whitelist_distr_flats.Id_district=1) AND (offers.Room_counts=bind_whitelist_distr_flats.Room_counts OR bind_whitelist_distr_flats.Room_counts=0) inner join types on offers.Id_type=types.Id_type inner join flat_types on offers.Id_flat_type=flat_types.Id_flat_type INNER JOIN localities ON offers.Id_locality=localities.Id_locality inner join districts on offers.Id_district=districts.Id_district " . 
 										"where bind_whitelist_distr_flats.Id_whitelist_user=" . $row[0] . " AND (";
 										
-										if(row[3]==1 && row[4]==1) 
+										if(row[3]==true && row[4]==true) 
 										{
 											$query = $query . "offers.IsNew=1 OR offers.IsEdit=1);";
 											echo "first<br>";
 										}
-										if(row[3]==1 && row[4]==0) 
+										if(row[3]==true && row[4]==false) 
 										{
 											$query = $query . "offers.IsNew=1);";
 											echo "second<br>";
 										}
-										if(row[4]==1 && row[3]==0) 
+										if(row[4]==true && row[3]==false) 
 										{
 											$query = $query . "offers.IsEdit=1);";
 											echo "third<br>";
