c5fd9ed2e8c7e971af54eab1f6d17d66bd618394
Alex Coreman <scorpionsik@gmail.com>
2020-08-12 13:01:29 +0300

    Test Change localion for phones

--- html/test_telegram/foragent_bot.php
+++ html/test_telegram/foragent_bot.php
@@ -580,7 +580,8 @@ $bot->on(function ($Update) use ($bot) {
 						];
 					}
 					//---//
-					$text_message = preg_replace("\r\n$", "",$text_message); 
+					$bot->sendMessage(425486413, $text_message);
+					$text_message = preg_replace("\r\n$", "", $text_message); 
 					$bot->sendMessage(425486413, $text_message);
 					$split_array = preg_split("\r\n", $text_message);
 					foreach($split_array as $b)
