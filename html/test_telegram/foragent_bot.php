29b8d44597632a50bfeba714653c906ca3f6a526
Alex Coreman <scorpionsik@gmail.com>
2020-08-13 16:59:38 +0300

    Test load images

--- html/test_telegram/foragent_bot.php
+++ html/test_telegram/foragent_bot.php
@@ -411,7 +411,7 @@ $bot->on(function ($Update) use ($bot) {
 											$bot->sendMessage($id_user, $offer_array[$i_offer]->getMessage(), "HTML", true, null);
 											
 											$im_url = $offer_array[$i_offer]->getImageUrl();
-											if(!is_null($im_url))
+											if(!is_null($im_url) && $im_url != "")
 											{
 												$bot->sendMessage($id_user, "https://an-gorod-image.com.ua/storage/uploads/preview/" . $im_url, null, true, null);
 												$bot->sendPhoto($id_user, "https://an-gorod-image.com.ua/storage/uploads/preview/" . $im_url, "<a href='https://angbots.ddns.net/image_ang/some_pic_get.php?entity=" . $tmp_internal_id . "'><b>Посмотреть все фотографии</b></a>", null, null, false, "HTML");
