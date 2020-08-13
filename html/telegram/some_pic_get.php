6cb799def56df734b3356ae511a929900c66d58e
Alex Coreman <scorpionsik@gmail.com>
2020-08-13 14:16:21 +0300

    Start add photos

--- html/telegram/some_pic_get.php
+++ html/telegram/some_pic_get.php
@@ -3,6 +3,6 @@
 	{
 		$entity_id = htmlentities($_GET['entity']);
 		$json_photos = file_get_contents("https://an-gorod-image.com.ua/api/adimages?code=${entity_id}&key=YbKhGDGFn67nHgl5fhFdflr5jGg3kg");
-		echo $json_photos;
+		echo json_decode($json_photos);
 	}
 ?>
