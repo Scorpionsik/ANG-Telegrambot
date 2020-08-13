0ccb68ab82321070b513db5d4c4c3a51adf5e138
Alex Coreman <scorpionsik@gmail.com>
2020-08-13 14:10:32 +0300

    Start add photos

--- html/telegram/some_pic_get.php
+++ html/telegram/some_pic_get.php
@@ -1,7 +1,7 @@
 <?php
 	if (isset($_GET['entity']))
 	{
-		$entity_id = htmlentities($_POST['entity']);
+		$entity_id = htmlentities($_GET['entity']);
 		echo $entity_id;
 		echo file_get_contents("https://an-gorod-image.com.ua/api/adimages?code=${entity_id}&key=YbKhGDGFn67nHgl5fhFdflr5jGg3kg");
 	}
