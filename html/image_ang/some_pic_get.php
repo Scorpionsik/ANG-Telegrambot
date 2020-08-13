84a0d84766f0fa689dc034dd2d6f348c762800b4
Alex Coreman <scorpionsik@gmail.com>
2020-08-13 14:51:31 +0300

    Change pic destination

--- html/image_ang/some_pic_get.php
+++ html/image_ang/some_pic_get.php
@@ -5,12 +5,14 @@ include "givemyprecious.php";
 	{
 		$entity_id = htmlentities($_GET['entity']);
 		$json_photos = file_get_contents("https://an-gorod-image.com.ua/api/adimages?code=${entity_id}&key=${token}");
-		$array_pic = json_decode($json_photos);
+		$arrays = json_decode($json_photos);
 		
-		foreach($array_pic as $key => $pic_path)
+		foreach($arrays as $array)
 		{
-			if($key == "origin")
-				echo $pic_path . "<br>";
+			foreach($array as $key => $value)
+			{
+				if($key == "origin") echo $pic_path . "<br>";
+			}
 		}
 	}
 ?>
