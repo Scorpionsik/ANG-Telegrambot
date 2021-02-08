f04505c4e154d53fe3859d9ea3db05a174e1156f
Alex Coreman <scorpionsik@gmail.com>
2021-02-08 16:04:41 +0200

    Test: Add Id_whitelist and Mode_value to RequestInfo

--- html/test_telegram/RequestInfo.php
+++ html/test_telegram/RequestInfo.php
@@ -3,38 +3,54 @@ namespace ANGBot;
 
 class RequestInfo{
 	private $id_telegram;
-	private $message_text;
+	private $id_whitelist;
+	private $message_data;
 	private $callback_data;
+	private $mode_value;
 
-	public function __construct($update){
-
+	public function __construct($update, $id_whitelist = null, $mode_value = 0){
 		if(is_a($update, '\TelegramBot\Api\Types\Message')){
 			$this->id_telegram = $update->getChat()->getId();
-			$this->message_text = $update;
+			$this->$message_data = $update;
 		}
 		else if(is_a($update, '\TelegramBot\Api\Types\Update')){
-			$this->message_text = $update->getMessage();
-			if(is_null($this->message_text)){
+			$this->$message_data = $update->getMessage();
+			if(is_null($this->$message_data)){
 				$callback = $update->getCallbackQuery();
 				if(!is_null($callback)){
 					$this->callback_data = $callback->getData();
-					$this->message_text = $callback->getMessage();
+					$this->$message_data = $callback->getMessage();
 				}
 			}
-			$this->id_telegram = $this->message_text->getChat()->getId();
+			$this->id_telegram = $this->$message_data->getChat()->getId();
 		}
+		else if(is_a($update, '\ANGBot\RequestInfo')){
+			$this->id_telegram = $update->getIdTelegram();
+			$this->message_data = $update->getMessageData();
+			$this->callback_data = $update->getCallbackData();
+		}
+		$this->id_whitelist = $id_whitelist;
+		$this->mode_value = $mode_value;
 	}
 
 	public function getIdTelegram(){
 		return $this->id_telegram;
 	}
 
-	public function getMessageText(){
-		return $this->message_text;
+	public function getMessageData(){
+		return $this->$message_data;
 	}
 
 	public function getCallbackData(){
 		return $this->callback_data;
+	}
+
+	public function getIdWhitelist(){
+		return $this->$id_whitelist;
+	}
+
+	public function getModeValue(){
+		return $this->mode_value;
 	}
 }
 ?>
