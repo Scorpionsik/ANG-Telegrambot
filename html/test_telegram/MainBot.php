d546cddfae01797fc3b13d6d16fa99a5e51bae91
Alex Coreman <scorpionsik@gmail.com>
2021-02-08 16:05:08 +0200

    Test: Upgrade MainBot and add start command

--- html/test_telegram/MainBot.php
+++ html/test_telegram/MainBot.php
@@ -5,6 +5,7 @@ namespace ANGBot;
 $root_dir = explode('html',__DIR__)[0] . 'html';
 require_once $root_dir . "/vendor/autoload.php";
 include "RequestInfo.php";
+include "connection_agent.php";
 
 class MainBot{
 	private $bot;
@@ -14,11 +15,22 @@ class MainBot{
 	public function __construct($bot_token){
 		$this->bot = new \TelegramBot\Api\Client($bot_token);
 
+		$this->bot->command('start', function ($message) {
+			$request_info = $this->getFullInfo(new \ANGBot\RequestInfo($message));
+			$this->sendMessage($request_info->getIdTelegram(), $request_info->getIdTelegram() . ": " . $request_info->getIdWhitelist() . ", " . $request_info->getModeValue());
+		}
+
 		$this->bot->command('help', function ($message) {
-			$this->commandHelp(new \ANGBot\RequestInfo($message));
+			$this->commandHelp($message->getChat()->getId());
 		});
 
 		$this->bot->run();
+
+		$this->db = new mysqli($host, $dblogin, $dbpassw, $database);
+	}
+
+	function __destruct(){
+		$this->dispose();
 	}
 
 	//отправка сообщений
@@ -26,11 +38,37 @@ class MainBot{
 		$this->bot->sendMessage($id_telegram, $message_text, 'HTML');
 	}
 
-	//логика команды help
-	private function commandHelp($request_info){
-		$this->sendMessage($request_info->getIdTelegram(), 'help');
+	//Получает RequestInfo с информацией о id_whitelist пользователя; если пользователя не было в базе данных, добавляет его
+	private function getFullInfo($request_info){
+		$return = $request_info;
+		$query = "SELECT * FROM telegram_users  where Id_telegram_user=". $request_info->getIdTelegram() .";";
+		$result = mysqli_query($this->db, $query) or die("Ошибка " . mysqli_error($this->db));
+		if($result){
+			$row_check = mysqli_num_rows($result);
+			if($row_check == 0){
+				$query = "INSERT INTO telegram_users (Id_telegram_user) values (". $request_info->getIdTelegram() .");";
+				mysqli_query($this->db, $query) or die("Ошибка " . mysqli_error($this->db));
+			}
+			else{
+				$row = mysqli_fetch_row($result);
+				if($row){
+					$return = new \ANGBot\RequestInfo($request_info, $row[1], $row[4]);
+				}
+				
+			}
+			mysqli_free_result($result);
+		}
+		return $return;
+	}
+
+	//выводит сообщение о помощи
+	private function commandHelp($id_telegram){
+		$this->sendMessage($id_telegram, 'help <b>me</b>');
+	}
+
+	private function dispose(){
+		mysqli_close($this->db);
 	}
 }
-
 
 ?>
