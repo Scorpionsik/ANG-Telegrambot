b64c077690feb1499dd40ff6d17259cc4a53f47c
Alex Coreman <scorpionsik@gmail.com>
2021-02-09 11:28:22 +0200

    Test: Fix bug in MainBot

--- html/test_telegram/MainBot.php
+++ html/test_telegram/MainBot.php
@@ -10,7 +10,7 @@ include __DIR__ . "/Modules/TestBotModule.php";
 class MainBot{
 	private $bot;
 	private $db;
-	private const $id_admin = 780925203;
+	private static $id_admin = 780925203;
 
 	//инициализация бота
 	public function __construct($bot_token){
