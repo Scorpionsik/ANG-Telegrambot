d669dd3d026548db39314dd53147fc0af4e7342e
Alex Coreman <scorpionsik@gmail.com>
2021-02-10 14:41:11 +0200

    Test: Fix bugs in BotModule

--- html/test_telegram/Modules/BotModule.php
+++ html/test_telegram/Modules/BotModule.php
@@ -10,7 +10,7 @@ abstract class BotModule{
 	public function start($request_info, $whitelist_info){
 		try{
 			if(is_null($request_info->getCallbackData())) $this->forMessages($request_info, $whitelist_info);
-			else $this->forCallbacks($request_info, $whitelist_info)
+			else $this->forCallbacks($request_info, $whitelist_info);
 		}
 		catch(Exception $e){
 			$this->main_bot->sendException($e, $request_info, $whitelist_info);
