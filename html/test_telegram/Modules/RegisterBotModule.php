b42d83c7af7a4466158c02bbcf082313677941f9
Alex Coreman <scorpionsik@gmail.com>
2021-02-10 16:01:53 +0200

    Test: Try add keyboard in access message in RegisterModule

--- html/test_telegram/Modules/RegisterBotModule.php
+++ html/test_telegram/Modules/RegisterBotModule.php
@@ -1,5 +1,7 @@
 <?php
+$keyboard_dir = explode('test_telegram',__DIR__)[0] . 'test_telegram/Keyboards';
 require_once "BotModule.php";
+require_once $keyboard_dir . "/DefaultBotKeyboard.php";
 
 class RegisterBotModule extends BotModule{
 	//сообщение ошибки по умолчанию
@@ -98,7 +100,7 @@ class RegisterBotModule extends BotModule{
 		$this->main_bot->sendMessage($id_telegram, "Здравствуйте, ${username}!");
 		$this->main_bot->sendMessage($id_telegram, "Ваша личность подтверждена! Вы подписаны на обновления по вашему району, они будут приходить вам в течении дня автоматически!");
 		$this->main_bot->sendMessage($id_telegram, "Если в уведомлениях вам нужны <b>только новые объявления</b>, нажмите на кнопку ниже - <b>❕ Присылать только новые объекты в уведомлениях</b>.");
-		$this->main_bot->sendMessage($id_telegram, "Чтобы получить всю информацию по вашему району за последние 3 дня, нажмите кнопку ниже.");
+		$this->main_bot->sendMessage($id_telegram, "Чтобы получить всю информацию по вашему району за последние 3 дня, нажмите кнопку ниже.", new DefaultBotKeyboard());
 	}
 }
 
