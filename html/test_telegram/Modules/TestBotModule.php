e2c810ee21f265d2a8ceef57397885851f946fd3
Alex Coreman <scorpionsik@gmail.com>
2021-02-10 17:11:05 +0200

    Test: Try add empty keyboard

--- html/test_telegram/Modules/TestBotModule.php
+++ html/test_telegram/Modules/TestBotModule.php
@@ -3,9 +3,10 @@ $keyboard_dir = explode('test_telegram',__DIR__)[0] . 'test_telegram/Keyboards';
 require_once $keyboard_dir . "/BotKeyboard.php";
 require_once $keyboard_dir . "/KeyboardButton.php";
 require_once "BotModule.php";
-private $tmp_keyboard;
 
 class TestBotModule extends BotModule{
+	private $tmp_keyboard;
+	
 	public function __construct($main_bot){
 		parent::__construct($main_bot);
 		
