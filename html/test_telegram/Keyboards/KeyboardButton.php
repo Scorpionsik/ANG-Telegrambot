7f056ac8fdcd62420862169e28a70395a048064b
Alex Coreman <scorpionsik@gmail.com>
2021-02-10 16:51:54 +0200

    Test: Debug keyboard

--- html/test_telegram/Keyboards/KeyboardButton.php
+++ html/test_telegram/Keyboards/KeyboardButton.php
@@ -7,13 +7,8 @@ class KeyboardButton{
 		$button_array = array();
 	}
 	
-	public function addData($param, $value, $position = -1, $is_overwrite = false){
-		$new_data = array($param => $value);
-		if($position < 0) $this->button_array[] = $new_data;
-		else{
-			if($is_overwrite) $this->button_array[$position] = $new_data;
-			else array_splice($this->button_array, $position, 0, $new_data);
-		}
+	public function addData($param, $value){
+		$this->button_array[$param] = $value;
 	}
 	
 	public function getButtonArray(){
