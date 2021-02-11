de6777d98c8cfe4fe9135f422c0800b6153da991
Alex Coreman <scorpionsik@gmail.com>
2021-02-11 11:27:49 +0200

    Test: Change constructor in DefaultKeyboard

--- html/test_telegram/Keyboards/DefaultBotKeyboard.php
+++ html/test_telegram/Keyboards/DefaultBotKeyboard.php
@@ -3,13 +3,13 @@ require_once "BotKeyboard.php";
 require_once "KeyboardButton.php";
 
 class DefaultBotKeyboard extends BotKeyboard{
-	public function __construct($is_get_only_new_offers = false){
+	public function __construct($is_get_edit_offers = true){
 		parent::__construct(2);
 		
 		$button = new KeyboardButton("📥 Получить всё за последние 3 дня");
 		$this->addButton($button, 1);
 		
-		if($is_get_only_new_offers) $button = new KeyboardButton("✅ Получать все объекты в уведомлениях");
+		if(!$is_get_edit_offers) $button = new KeyboardButton("✅ Получать все объекты в уведомлениях");
 		else $button = new KeyboardButton("❕ Присылать только новые объекты в уведомлениях");
 		$this->addButton($button, 0);
 		
