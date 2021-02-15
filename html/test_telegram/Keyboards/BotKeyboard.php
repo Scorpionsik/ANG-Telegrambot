9515e1575afcd5cc0078ba3cd348c4dd9d4fee66
Alex Coreman <scorpionsik@gmail.com>
2021-02-15 10:57:11 +0200

    Test: Update BotKeyboard and fix bugs

--- html/test_telegram/Keyboards/BotKeyboard.php
+++ html/test_telegram/Keyboards/BotKeyboard.php
@@ -10,15 +10,21 @@ class BotKeyboard{
 		}
 	}
 	
-	public function addRow(){
-		$this->keyboard_array[] = array();
+	public function addRow($position = -1, $is_overwrite = false){
+		if($position < 0 && $position > count($this->keyboard_array) - 1) $this->keyboard_array[] = array();
+		else{
+			if($is_overwrite) $this->keyboard_array[$position] = array();
+			else array_splice($this->keyboard_array, $position, 0, array());
+		}
 	}
 	
-	public function addButton($keyboard_button, $index_row, $position = -1, $is_overwrite = false){
-		if($position < 0) $this->keyboard_array[$index_row][] = $keyboard_button->getButtonArray();
+	public function addButton($keyboard_button, $index_row = -1, $position = -1, $is_overwrite = false){
+		$current_index_row = $index_row;
+		if($index_row < 0) $current_index_row = count($this->keyboard_array) - 1;
+		if($position < 0) $this->keyboard_array[$current_index_row][] = $keyboard_button->getButtonArray();
 		else{
-			if($is_overwrite) $this->keyboard_array[$index_row][$position] = $keyboard_button->getButtonArray();
-			else array_splice($this->keyboard_array, $position, 0, $keyboard_button->getButtonArray());
+			if($is_overwrite) $this->keyboard_array[$current_index_row][$position] = $keyboard_button->getButtonArray();
+			else array_splice($this->keyboard_array[$current_index_row], $position, 0, $keyboard_button->getButtonArray());
 		}
 	}
 	
