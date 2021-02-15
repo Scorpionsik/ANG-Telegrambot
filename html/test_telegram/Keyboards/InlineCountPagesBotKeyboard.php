0252a6413e2432a5120c2fa51877db39c232cb11
Alex Coreman <scorpionsik@gmail.com>
2021-02-15 12:09:13 +0200

    Test: Debug MainModule without callbacks

--- html/test_telegram/Keyboards/InlineCountPagesBotKeyboard.php
+++ html/test_telegram/Keyboards/InlineCountPagesBotKeyboard.php
@@ -3,7 +3,7 @@ require_once "BotKeyboard.php";
 require_once "KeyboardButton.php";
 
 class InlineCountPagesBotKeyboard extends BotKeyboard {
-	private $max_pages_in_keyboard = 9;
+	private $max_pages_in_keyboard = 8;
 	
 	public function __construct($current_page, $count_pages){
 		parent::__construct(1);
