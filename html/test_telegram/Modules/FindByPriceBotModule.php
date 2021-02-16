78e77822c09eec4233636212aac5643b5da98dcf
Alex Coreman <scorpionsik@gmail.com>
2021-02-16 10:58:06 +0200

    Test: Try added FindByPriceModule

--- html/test_telegram/Modules/FindByPriceBotModule.php
+++ html/test_telegram/Modules/FindByPriceBotModule.php
@@ -11,26 +11,29 @@ class FindByPriceBotModule extends BotModule{
 	public function __construct($main_bot){
 		parent::__construct($main_bot);
 		
-		$this->default_keyboard = new BotKeyboard(1);
-		$button = new KeyboardButton("Отмена");
-		$this->default_keyboard->addButton($button);
+		$this->default_keyboard = new BotKeyboard(2);
+		$this->default_keyboard->addButton(new KeyboardButton("Сбросить цену"), 0);
+		$this->default_keyboard->addButton(new KeyboardButton("Отмена"), 1);
 	}
 	
 	protected function forMessages($request_info, $whitelist_info){
 		if($request_info->getModeParam() == 0){
-			$this->main_bot->changeMode($request_info, $whitelist_info, 1, 1, false);
+			$this->changeModeParam($request_info, $whitelist_info, 1);
 		}
 		else{
 			$message_text = $this->main_bot->getMessageText($request_info->getMessageData());
 			if(preg_match('/^\d+$/', $message_text)){
-				$query = "update bind_whitelist_distr_flats set Price_lower_than=". $message_text ." where Id_whitelist_user=" . $whitelist_info->getIdWhitelist() . ";";
-				$this->main_bot->getRequestResult($query);
+				$this->changeFindByPrice($message_text, $whitelist_info);
+				$this->exitModule($request_info, $whitelist_info);
+			}
+			else if($message_text == "Сбросить цену"){
+				$this->changeFindByPrice(0, $whitelist_info);
 				$this->exitModule($request_info, $whitelist_info);
 			}
 			else if($message_text == "Отмена") $this->exitModule($request_info, $whitelist_info);
 			else $this->main_bot->sendMessage($request_info->getIdTelegram(), "Введено неверное значение!");
 		}
-		if(!$this->lock) $this->main_bot->sendMessage($request_info->getIdTelegram(), "Введите цену <u>без пробелов</u>. Бот найдёт и отобразит объекты с такой же ценой или ниже. Чтобы убрать фильтр по цене, <b>введите 0</b>.", $this->default_keyboard);
+		if(!$this->lock) $this->main_bot->sendMessage($request_info->getIdTelegram(), "Введите цену <u>без пробелов</u>. Бот найдёт и отобразит объекты с такой же ценой или ниже. Чтобы убрать фильтр по цене, <b>введите 0</b> или нажмите кнопку <b>Сбросить цену</b>", $this->default_keyboard);
 		
 	}
 	
@@ -41,6 +44,11 @@ class FindByPriceBotModule extends BotModule{
 	private function exitModule($request_info, $whitelist_info){
 		$this->lock = true;
 		$this->resetToDefaultMode($request_info, $whitelist_info, true);
+	}
+	
+	private function changeFindByPrice($value, $whitelist_info){
+		$query = "update bind_whitelist_distr_flats set Price_lower_than=". $value ." where Id_whitelist_user=" . $whitelist_info->getIdWhitelist() . ";";
+		$this->main_bot->getRequestResult($query);
 	}
 }
 
