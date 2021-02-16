72dd0db9771c86ccfc9c753677a9e426fd076946
Alex Coreman <scorpionsik@gmail.com>
2021-02-16 16:41:19 +0200

    Apply changes for notifications

---
+++ html/telegram/NotificationBot.php
@@ -0,0 +1,121 @@
+<?php
+$root_dir = explode('html',__DIR__)[0] . 'html';
+require_once $root_dir . "/vendor/autoload.php";
+require_once "Functions.php";
+require_once "WhitelistInfo.php";
+require_once "WhitelistUser.php";
+require_once __DIR__ . "/Keyboards/InlineOfferBotKeyboard.php";
+
+class NotificationBot{
+	private $bot;
+	private $db;
+	private $functions;
+	
+	public function __construct($bot_token){
+		include "connection_agent.php";
+		$this->db = new mysqli($host, $dblogin, $dbpassw, $database);
+		$this->bot = new \TelegramBot\Api\Client($bot_token);
+		$this->functions = new Functions();
+	}
+	
+		//очистка данных
+	function __destruct(){
+		$this->dispose();
+	}
+	
+	public function getWhitelistUsers($single_user = null){
+		$return = array();
+		$query_part = "";
+		if(!is_null($single_user)) $query_part = " and Id_whitelist_user = ${single_user}";
+		$query = "select telegram_users.Id_whitelist_user as 'Id', telegram_users.Id_telegram_user as 'Telegram', white_list.Is_accept_base_button, white_list.Is_get_new_offers, white_list.Is_get_edit_offers from telegram_users join white_list using (Id_whitelist_user) WHERE white_list.Is_banned != 1 and white_list.Is_locked != 1${query_part} and (white_list.Is_get_new_offers=1 or white_list.Is_get_edit_offers=1);";
+		$result = $this->getRequestResult($query);
+		if($result){
+			$row_check = mysqli_num_rows($result);
+			if($row_check > 0){ 
+				for($i = 0; $i < $row_check; $i++){
+					$row = mysqli_fetch_row($result);
+					$whitelist_info = new WhitelistInfo($row[0], null, null, null, null, $row[2], $row[3], $row[4], null);
+					$whitelist_user = new WhitelistUser($row[1], $whitelist_info);
+					$return[] = $whitelist_user;
+				}
+				
+			}
+			mysqli_free_result($result);
+		}
+		return $return;
+	}
+	
+	public function getOffersForWhitelistUser($whitelist_user){
+		$query_part = "";
+		if($whitelist_user->getWhitelistInfo()->getIsGetNewOffers() == 1 || $whitelist_user->getWhitelistInfo()->getIsGetEditOffers() == 1){
+			$query_part = " and (";
+			if($whitelist_user->getWhitelistInfo()->getIsGetNewOffers() == 1){
+				$query_part = $query_part . "offers.IsNew=1";
+				if($whitelist_user->getWhitelistInfo()->getIsGetEditOffers() == 1){
+					$query_part = $query_part . " or ";
+				}
+			}
+			if($whitelist_user->getWhitelistInfo()->getIsGetEditOffers() == 1){
+				$query_part = $query_part . "offers.IsEdit=1";
+			}
+			$query_part = $query_part . ")";
+		}
+		
+		$query = "where bind_whitelist_distr_flats.Id_whitelist_user=" . $whitelist_user->getWhitelistInfo()->getIdWhitelist() . "${query_part};";
+		return $this->getOffers($query);
+	}
+	
+	public function showOffer($offer, $id_telegram, $whitelist_info){
+		$inline_offer_keyboard = new InlineOfferBotKeyboard($offer, $whitelist_info);
+		//основное сообщение
+		$this->sendMessage($id_telegram, $offer->getOfferDescription());
+		//фотографии
+		if(!is_null($offer->getImageUrl()) && $offer->getImageUrl() != ""){
+			try{
+				$this->sendPhoto($id_telegram, "https://an-gorod-image.com.ua/storage/uploads/preview/" . $offer->getImageUrl(), "<a href='https://angbots.ddns.net/image_ang/some_pic_get.php?entity=" . $offer->getIdOffer() . "'><b>Посмотреть все фотографии</b></a>");
+			}
+			catch(Exception $e){
+			}
+		}
+		//место под телефоны и инлайн клаву
+		$this->sendMessage($id_telegram, "Чтобы посмотреть контакты владельца объекта <b>". $offer->getIdOffer() ."</b>, нажмите на кнопку 'Телефоны' ниже.", $inline_offer_keyboard, true);
+	}
+	
+	private function getOffers($where_query_part){
+		$result = $this->getRequestResult($this->functions->getSelectAndFromQueryPart() . $where_query_part);
+		$offers_array = $this->functions->getOffersFromDBResult($result);
+		mysqli_free_result($result);
+		return $offers_array;
+	}
+	
+	//получить результат запроса из базы данных
+	private function getRequestResult($query){
+		$result = mysqli_query($this->db, $query) or die("Ошибка " . mysqli_error($this->db));
+		return $result;
+	}
+	
+		//отправка сообщений в телеграм-чат
+	private function sendMessage($id_telegram, $message_text, $bot_keyboard = null){
+		$keyboard = null;
+		if(!is_null($bot_keyboard)){
+				$keyboard = $this->getInlineKeyboard($bot_keyboard);
+		}
+		$this->bot->sendMessage($id_telegram, $message_text, 'HTML', true, null, $keyboard);
+	}
+	
+	private function sendPhoto($id_telegram, $photo_link, $description){
+		$this->bot->sendPhoto($id_telegram, $photo_link, $description, null, null, false, "HTML");
+	}
+	
+	private function getInlineKeyboard($bot_keyboard){
+		$return = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup($bot_keyboard->getKeyboardArray());
+		return $return;
+	}
+	
+	//закрывает подключение к базе данных
+	private function dispose(){
+		mysqli_close($this->db);
+	}
+}
+
+?>
--- html/telegram/foragent_send_notifications.php
+++ html/telegram/foragent_send_notifications.php
@@ -1,159 +1,20 @@
 <?php
-$root_dir = explode('html',__DIR__)[0] . 'html';
-
 include "givemyprecious.php";
-include "connection_agent.php";
-require_once $root_dir . "/vendor/autoload.php";
+include "NotificationBot.php";
 
-$dblink = new mysqli($host, $dblogin, $dbpassw, $database); 
-$bot = new \TelegramBot\Api\Client($token);
+$bot = new NotificationBot($token_test);
 
-/*
-0	telegram_users.Id_whitelist_user 	int
-1	telegram_users.Id_telegram_user 	int
-2	white_list.Is_accept_base_button	boolean
-3	white_list.Is_get_new_offers		boolean
-4	white_list.Is_get_edit_offers		boolean
-5	telegram_users.IsExist				boolean
-*/
-$query = "select telegram_users.Id_whitelist_user as 'Id', telegram_users.Id_telegram_user as 'Telegram', white_list.Is_accept_base_button, white_list.Is_get_new_offers, white_list.Is_get_edit_offers, telegram_users.IsExist from telegram_users join white_list using (Id_whitelist_user) WHERE telegram_users.Id_whitelist_user != 11 && white_list.Is_Banned != 1;";
-$result = mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
-if($result)
-{
-	//код выдачи данных
-	include "foragent_functions.php";
-	
-	$count = mysqli_num_rows($result);
-	for($i = 0; $i < $count; $i++)
-	{
-		$row = mysqli_fetch_row($result);
-		
-		
-		if($row)
-		{
-			$id_whitelist = $row[0];
-			$is_accept_base_button = $row[2];
-			$is_new = $row[3];
-			$is_edit = $row[4];
-			$is_exist = $row[5];
-						
-			if($is_new == 1 || $is_edit == 1)
-			{
-				$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup(makeArrayForDefaultKeyboard($is_edit),
-					false,
-					true);
-					
-				$id_user = $row[1];
-				
-				//show results code
-				
-				$query = "";
-				if($is_new == 1) 
-				{
-					$query = $query . "offers.IsNew=1";
-					if($is_edit == 1)
-					{
-						$query = $query . " or ";
-					}
-				}
-				if($is_edit == 1) 
-				{
-					$query = $query . "offers.IsEdit=1";
-				}
-				
-				$offer_array = makeOfferMessages($dblink, $id_whitelist, $query);
-				$count_offer_array = count($offer_array);
-				//если для агента есть информация
-				if($count_offer_array > 0)
-				{
-					foreach($offer_array as $offer)
-					{
-						$tmp_internal_id = $offer->getInternalId();
-						//полная инлайн клавиатура
-						$keyboard_inline = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
-							[
-								[
-									['text' => '🛄 Объект на сайте', 'url' => 'http://an-gorod.com.ua/real/flat/sale?q=' . $tmp_internal_id],['text' => '💼 Объект в базе', 'url' => 'http://newcab.bee.th1.vps-private.net/node/' . $offer->getEntityId()]
-								],[
-									['text' => '☎️ Телефоны', 'callback_data' => $tmp_internal_id]
-								]
-							]
-						);
-						
-						//---проверка доступа к кнопке "Объект в базе"---//
-						if($is_accept_base_button == 0)
-						{
-								//инлайн клавиатура без кнопки "Объект в базе"
-							$keyboard_inline = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
-								[
-									[
-										['text' => '🛄 Объект на сайте', 'url' => 'http://an-gorod.com.ua/real/flat/sale?q=' . $tmp_internal_id]
-									],[
-										['text' => '☎️ Телефоны', 'callback_data' => $tmp_internal_id]
-									]
-								]
-							);
-						}
-						//---конец проверка доступа к кнопке "Объект в базе"---//
-						
-						//Выбивало ошибку, что не может отправить агенту. Возможно, удалил бота у себя. Проверить и фиксировать
-						try{
-							$google_map_keyboard = null;
-							
-							$country = $offer->getCountry();
-							$address = $offer->getAddress();
-							$house_num = $offer->getHouseNum();
-							
-							if($country != null && $address != null && $house_num != null)
-							{
-								$country = preg_replace('/[ ]/','+',$country);
-								$address = preg_replace('/[ ]/','+',$address);
-								$house_num = preg_replace('/[ ]/','+',$house_num);
-								$google_map_keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup([[['text'=>'🗺 Посмотреть на карте', 'url'=>'https://www.google.com.ua/maps/place/' . $address . "," . $house_num . "," . $country]]]);
-							}
-							
-							$bot->sendMessage($id_user, $offer->getMessage(), "HTML", true, null, $google_map_keyboard);
-							//$bot->sendMessage($id_user, $offer->getMessage(), "HTML", true, null);
-							$im_url = $offer->getImageUrl();
-							if(!is_null($im_url) && $im_url != "")
-							{
-								try
-								{
-									$bot->sendPhoto($id_user, "https://an-gorod-image.com.ua/storage/uploads/preview/" . $im_url, "<a href='https://angbots.ddns.net/image_ang/some_pic_get.php?entity=" . $tmp_internal_id . "'><b>Посмотреть все фотографии</b></a>", null, null, false, "HTML");
-								}
-								catch (Exception $e)
-								{
-									
-								}
-							}
-							$bot->sendMessage($id_user, "Чтобы посмотреть контакты владельца объекта ${tmp_internal_id}, нажмите на кнопку 'Телефоны' ниже.", null, true, null, $keyboard_inline, true);
-						}	
-						catch (Exception $e)
-						{
-							break;
-						}
-					}
-					
-					//Выбивало ошибку, что не может отправить агенту. Возможно, удалил бота у себя. Проверить и фиксировать
-					try{
-						$bot->sendMessage($id_user, declOfNum($count_offer_array,array('объект пришел','объекта пришло','объектов пришло')) . " за последние пару минут.", null, false, null, $keyboard);
-						/*if($is_exist == 0){
-							$query = "update telegram_users set IsExist=1 where telegram_users.Id_whitelist_user=" . $id_whitelist . ";";
-							mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));
-						}*/
-					}	
-					catch (Exception $e) 
-					{
-						/*$query = "update telegram_users set IsExist=0 where telegram_users.Id_whitelist_user=" . $id_whitelist . ";";
-						mysqli_query($dblink, $query) or die("Ошибка " . mysqli_error($dblink));*/
-					}
-				}
-			}
+$whitelist_users_array = $bot->getWhitelistUsers(10);
+foreach($whitelist_users_array as $whitelist_user){
+	$offers_array = $bot->getOffersForWhitelistUser($whitelist_user);
+	try{
+		foreach($offers_array as $offer){
+			$bot->showOffer($offer, $whitelist_user->getIdTelegram(), $whitelist_user->getWhitelistInfo());
 		}
 	}
-	mysqli_free_result($result);
+	catch(Exception $ex){
+	}
 }
-mysqli_close($dblink);
 
 echo date('l jS \of F Y h:i:s A');
 ?>
