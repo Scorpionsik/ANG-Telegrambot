b09d74a7afbbad6021dcf736dcf5b6fa139c90c8
Scorpionsik <36457365+Scorpionsik@users.noreply.github.com>
2021-08-31 15:58:41 +0300

    Try get string in utf-8

--- html/test_telegram/Modules/MainBotModule.php
+++ html/test_telegram/Modules/MainBotModule.php
@@ -151,7 +151,7 @@ class MainBotModule extends BotModule{
 	    }
 	    
 	    //по району
-	    $pattern = "(*UTF8)/(\pL{3,}(?: \pL{3,})?)(?=\,)?/";
+	    $pattern = "(*UTF8)/(\pL{3,}(?: \pL{3,})?)(?=\,)?/u";
 	    if(preg_match('/(\pL{3,}(?: \pL{3,})?)(?=\,)?/i', $message_text, $matches)){
 	        $this->main_bot->callAdmin(count($matches));
 	        $search_params[] = "districts.District_name like (\"" . $matches[1] . "%\")"; //implode(" ; ", $matches);
