abce1f7c2f4899c99aab736132d72fc26128edf0
Scorpionsik <36457365+Scorpionsik@users.noreply.github.com>
2021-09-01 13:10:25 +0300

    Debug fix bug with multi-input

--- html/test_telegram/MainBot.php
+++ html/test_telegram/MainBot.php
@@ -32,7 +32,7 @@ class MainBot{
 					$request_info = new RequestInfo($Update);
 					$current_message_time = $request_info->getLastMessageDate();
 					$request_info = $this->getFullRequestInfo($request_info);
-					$time_currency = $request_info->getLastMessageDate() - $current_message_time;
+					$time_currency = $current_message_time - $request_info->getLastMessageDate();
 					$this->callAdmin($time_currency . " : " . $request_info->getLastMessageDate() . " - ${current_message_time}");
 					if($time_currency > 1){
 					    $this->getRequestResult("update telegram_users set Last_message_date=${current_message_time} where Id_telegram_user=" . $request_info->getIdTelegram() . ";");
